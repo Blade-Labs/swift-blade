b28ef426d5bf882740c51b21d0f6cf19da295ade
Ihor Dubii <ihor.dubii@unibrix.com>
2024-01-25 21:32:25 +0100

    WWT-3820: Provide ability to Stake on Hedera, using our SDK for Whitelabel Solutions

--- Sources/SwiftBlade/Models.swift
+++ Sources/SwiftBlade/Models.swift
@@ -97,7 +97,35 @@ public struct AccountInfoData: Codable {
     public var accountId: String
     public var evmAddress: String
     public var calculatedEvmAddress: String
+    public var publicKey: String
+    public var stakingInfo: StakingInfo
 }
+
+public struct StakingInfo: Codable {
+    public var pendingReward: Int64
+    public var stakedNodeId: Int64?
+    public var stakePeriodStart: String?
+}
+
+struct NodesResponse: Response, Codable {
+    var data: NodesData
+}
+
+public struct NodesData: Codable {
+    public var nodes: [NodeInfo]
+}
+
+public struct NodeInfo: Codable {
+    public var description: String
+    public var max_stake: Int64
+    public var min_stake: Int64
+    public var node_id: Int
+    public var node_account_id: String
+    public var stake: Int64
+    public var stake_not_rewarded: Int64
+    public var stake_rewarded: Int64
+}
+
 
 struct TransactionReceiptResponse: Response, Codable {
     var data: TransactionReceiptData
