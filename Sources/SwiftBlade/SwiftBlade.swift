84aeddcf798c69c33e746be551da33a1c7baa618
Ihor Dubii <ihor.dubii@unibrix.com>
2024-09-25 14:00:54 +0200

    WWT-5448: [SDK] fix problem with extra freezing transactions with paymaster (INVALID_NODE_ACCOUNT)

--- Sources/SwiftBlade/SwiftBlade.swift
+++ Sources/SwiftBlade/SwiftBlade.swift
@@ -15,7 +15,7 @@ public class SwiftBlade: NSObject {
     private var network: HederaNetwork = .TESTNET
     private var bladeEnv: BladeEnv = .Prod
     private var dAppCode: String?
-    private let sdkVersion: String = "Swift@0.6.33"
+    private let sdkVersion: String = "Swift@0.6.34"
 
     // MARK: - It's init time 🎬
 
